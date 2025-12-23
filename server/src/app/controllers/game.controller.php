284e54723cb2e453eaa2ca2d58943f30d4be9142
kfarkasHU <sohamar@farkask.eu>
2025-12-23 15:39:00 +0100

    feat(game controller): server side location handli ng

--- server/src/app/controllers/game.controller.php
+++ server/src/app/controllers/game.controller.php
@@ -5,11 +5,14 @@ class GameController {
     $stateService = new StateService();
     $chatMembersService = new ChatMembersService();
     $chatMessageService = new ChatMessagesService();
+    $maze = new Maze();
 
     $username = $stateService->getPlayerName();
+    $location = $maze->getPlayerInitialLocation();
     $currentHits = $stateService->getPlayerMaxHits();
 
     $chatMembersService->addMember($username);
+    $stateService->setPlayerPosition($location["x"], $location["y"]);
     $stateService->setPlayerCurHits($currentHits);
     $stateService->setChatLastMessageId($chatMessageService->getLastMessageId());
     $stateService->setEquipmentSword("SWORD_NORMAL");
@@ -39,14 +42,36 @@ class GameController {
     $this->sendBackState();
   }
 
+  public function maze() {
+    $maze = new Maze();
+
+    $map = $maze->getFullMap();
+    $location = $maze->getPlayerInitialLocation();
+    $tilesAround = $maze->getTilesAroundPlayer($location['x'], $location['y']);
+
+    echo json_encode([
+      "location" => $location,
+      "tilesAround" => $tilesAround,
+      "maze" => $map,
+      "size" => [
+        "width" => $maze->mazeWidth(),
+        "height" => $maze->mazeHeight(),
+      ]
+    ]);
+  }
+
   private function sendBackState() {
+    $maze = new Maze();
     $stateService = new StateService();
 
     $nextLevelInXp = 1000; // TODO
 
-    $sumXp = $stateService->getCharacterXp(0);
+    $sumXp = $stateService->getCharacterXp();
     $xpFromKills = $stateService->getCharacterXpFromKills();
     $xpFromKillsPercentage = $xpFromKills === 0 ? 0 : ($xpFromKills / $xpFromKills) * 100;
+
+    $location = $stateService->getPlayerPosition();
+    $tilesAround = $maze->getTilesAroundPlayer($location['x'], $location['y']);
 
     echo json_encode([
       "__debug" => $_SESSION,
@@ -101,144 +126,94 @@ class GameController {
           "label" => "[C]ast a spell"
         ]
       ),
-      "mapState" => [
-        "tile00" => [
-          "top" => "TILE_WALL",
-          "right" => "TILE_DOOR",
-          "bottom" => "TILE_OPEN",
-          "left" => "TILE_OPEN",
-          "occupiedBy" => [
-            "key" => "",
-          ],
-          "containsItems" => array(
-            [
-              "key" => "ITEM_GOLD",
-              "amount" => 10
-            ]
-          )
+      "mapState" => $this->calculateTiles($tilesAround)
+    ]);
+  }
+
+  private function calculateTiles(
+    $currentTiles
+  ): array {
+    return [
+      "tile00" => [
+          "top" => $this->getBorderType($currentTiles["tile01"]),
+          "right" => $this->getBorderType($currentTiles["tile12"]),
+          "bottom" => $this->getBorderType($currentTiles["tile21"]),
+          "left" => $this->getBorderType($currentTiles["tile12"]),
+          "occupiedBy" => null,
+          "containsItems" => array()
         ],
         "tile01" => [
-          "top" => "TILE_WALL",
-          "right" => "TILE_DOOR",
-          "bottom" => "TILE_OPEN",
-          "left" => "TILE_OPEN",
-          "occupiedBy" => [
-            "key" => "",
-          ],
-          "containsItems" => array(
-            [
-              "key" => "ITEM_GOLD",
-              "amount" => 10
-            ]
-          )
+          "top" => $this->getBorderType($currentTiles["tile02"]),
+          "right" => $this->getBorderType($currentTiles["tile13"]),
+          "bottom" => $this->getBorderType($currentTiles["tile22"]),
+          "left" => $this->getBorderType($currentTiles["tile11"]),
+          "occupiedBy" => null,
+          "containsItems" => array()
         ],
         "tile02" => [
-          "top" => "TILE_WALL",
-          "right" => "TILE_DOOR",
-          "bottom" => "TILE_OPEN",
-          "left" => "TILE_OPEN",
-          "occupiedBy" => [
-            "key" => "",
-          ],
-          "containsItems" => array(
-            [
-              "key" => "ITEM_GOLD",
-              "amount" => 10
-            ]
-          )
+          "top" => $this->getBorderType($currentTiles["tile03"]),
+          "right" => $this->getBorderType($currentTiles["tile14"]),
+          "bottom" => $this->getBorderType($currentTiles["tile23"]),
+          "left" => $this->getBorderType($currentTiles["tile12"]),
+          "occupiedBy" => null,
+          "containsItems" => array()
         ],
         "tile10" => [
-          "top" => "TILE_WALL",
-          "right" => "TILE_DOOR",
-          "bottom" => "TILE_OPEN",
-          "left" => "TILE_OPEN",
-          "occupiedBy" => [
-            "key" => "PLAYER",
-          ],
-          "containsItems" => array(
-            [
-              "key" => "ITEM_GOLD",
-              "amount" => 10
-            ]
-          )
+          "top" => $this->getBorderType($currentTiles["tile11"]),
+          "right" => $this->getBorderType($currentTiles["tile22"]),
+          "bottom" => $this->getBorderType($currentTiles["tile31"]),
+          "left" => $this->getBorderType($currentTiles["tile20"]),
+          "occupiedBy" => null,
+          "containsItems" => array()
         ],
         "tile11" => [
-          "top" => "TILE_WALL",
-          "right" => "TILE_DOOR",
-          "bottom" => "TILE_OPEN",
-          "left" => "TILE_OPEN",
+          "top" => $this->getBorderType($currentTiles["tile12"]),
+          "right" => $this->getBorderType($currentTiles["tile23"]),
+          "bottom" => $this->getBorderType($currentTiles["tile32"]),
+          "left" => $this->getBorderType($currentTiles["tile21"]),
           "occupiedBy" => [
-            "key" => "",
-          ],
-          "containsItems" => array(
-            [
-              "key" => "ITEM_GOLD",
-              "amount" => 10
-            ]
-          )
+            "key" => "PLAYER",
+          ]
         ],
         "tile12" => [
-          "top" => "TILE_WALL",
-          "right" => "TILE_DOOR",
-          "bottom" => "TILE_OPEN",
-          "left" => "TILE_OPEN",
-          "occupiedBy" => [
-            "key" => "",
-          ],
-          "containsItems" => array(
-            [
-              "key" => "ITEM_GOLD",
-              "amount" => 10
-            ]
-          )
+          "top" => $this->getBorderType($currentTiles["tile13"]),
+          "right" => $this->getBorderType($currentTiles["tile24"]),
+          "bottom" => $this->getBorderType($currentTiles["tile33"]),
+          "left" => $this->getBorderType($currentTiles["tile22"]),
+          "occupiedBy" => null,
+          "containsItems" => array()
         ],
         "tile20" => [
-          "top" => "TILE_WALL",
-          "right" => "TILE_DOOR",
-          "bottom" => "TILE_OPEN",
-          "left" => "TILE_OPEN",
-          "occupiedBy" => [
-            "key" => "",
-          ],
-          "containsItems" => array(
-            [
-              "key" => "ITEM_GOLD",
-              "amount" => 10
-            ]
-          )
+          "top" => $this->getBorderType($currentTiles["tile21"]),
+          "right" => $this->getBorderType($currentTiles["tile32"]),
+          "bottom" => $this->getBorderType($currentTiles["tile41"]),
+          "left" => $this->getBorderType($currentTiles["tile30"]),
+          "occupiedBy" => null,
+          "containsItems" => array()
         ],
         "tile21" => [
-          "top" => "TILE_WALL",
-          "right" => "TILE_DOOR",
-          "bottom" => "TILE_OPEN",
-          "left" => "TILE_OPEN",
-          "occupiedBy" => [
-            "key" => "",
-          ],
-          "containsItems" => array(
-            [
-              "key" => "ITEM_GOLD",
-              "amount" => 10
-            ]
-          )
+          "top" => $this->getBorderType($currentTiles["tile22"]),
+          "right" => $this->getBorderType($currentTiles["tile33"]),
+          "bottom" => $this->getBorderType($currentTiles["tile42"]),
+          "left" => $this->getBorderType($currentTiles["tile31"]),
+          "occupiedBy" => null,
+          "containsItems" => array()
         ],
         "tile22" => [
-          "top" => "TILE_WALL",
-          "right" => "TILE_DOOR",
-          "bottom" => "TILE_OPEN",
-          "left" => "TILE_OPEN",
-          "occupiedBy" => [
-            "key" => "",
-          ],
-          "containsItems" => array(
-            [
-              "key" => "ITEM_GOLD",
-              "amount" => 10
-            ]
-          )
-        ],
-      ]
-    ]);
+          "top" => $this->getBorderType($currentTiles["tile23"]),
+          "right" => $this->getBorderType($currentTiles["tile34"]),
+          "bottom" => $this->getBorderType($currentTiles["tile43"]),
+          "left" => $this->getBorderType($currentTiles["tile32"]),
+          "occupiedBy" => null,
+          "containsItems" => array()
+        ]
+      ];
+  }
+
+  private function getBorderType($targetTile): string {
+    if ($targetTile === "%") return "TILE_OPEN";
+    if ($targetTile === "#") return "TILE_WALL";
+    return "TILE_OPEN";
   }
 }
 ?>
