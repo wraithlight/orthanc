7ba0aac1c5415852e60fbf02a57b504bfc5702b7
kfarkasHU <sohamar@farkask.eu>
2025-12-25 00:22:14 +0100

    chore(run): improve DX by naming and autostop

--- server/run.sh
+++ server/run.sh
@@ -1,9 +1,14 @@
-docker build -t orthanc-server .
+#!/bin/bash
+CONTAINER_NAME=orthanc-server
 
+docker stop $CONTAINER_NAME >/dev/null 2>&1 || true
+docker rm $CONTAINER_NAME >/dev/null 2>&1 || true
+docker build -t $CONTAINER_NAME .
 docker run -it --rm \
   -d \
   -p 3100:80 \
   -v $(pwd)/src:/var/www/html \
   -v $(pwd)/data:/var/www/html/data \
   -v $(pwd)/game-data:/var/www/html/game-data \
-  orthanc-server
+  --name $CONTAINER_NAME \
+  $CONTAINER_NAME
