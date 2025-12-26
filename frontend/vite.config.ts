917b410eaebbb39fdaaadc81f8416ceb468e41c5
kfarkasHU <sohamar@farkask.eu>
2025-12-26 19:09:17 +0100

    chore(*): FE build

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,5 +1,6 @@
 import { defineConfig } from 'vite';
-import { resolve } from 'path';
+import { resolve, join } from 'path';
+import { readFileSync, writeFileSync } from 'fs';
 
 export default defineConfig({
   root: '.',
@@ -15,9 +16,16 @@ export default defineConfig({
     port: 3000,
     open: true,
   },
-  resolve: {
-    alias: {
-      '@': resolve(__dirname, 'src'),
-    },
-  },
+  plugins: [
+    {
+      name: "update-config-json",
+      apply: "build",
+      writeBundle() {
+        const configFilePath = resolve(__dirname, join("dist", "config.json"));
+        const content = JSON.parse(readFileSync(configFilePath, "utf-8"));
+        content.apiUrl = "";
+        writeFileSync(configFilePath, JSON.stringify(content), "utf-8");
+      }
+    }
+  ]
 });
