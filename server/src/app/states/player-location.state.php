cf12ebf49ee1dfbc4f8ccca59ea44e48ffcf9d53
kfarkasHU <sohamar@farkask.eu>
2026-01-02 00:07:27 +0100

    feat(player location state): add support for previous location

--- server/src/app/states/player-location.state.php
+++ server/src/app/states/player-location.state.php
@@ -6,6 +6,8 @@ class PlayerLocationState extends BaseState {
   private const LOCATION_CURRENT_Y_KEY = "PLAYER_LOCATION__LOCATION_CURRENT_Y_KEY";
   private const LOCATION_INITIAL_X_KEY = "PLAYER_LOCATION__LOCATION_INITIAL_X_KEY";
   private const LOCATION_INITIAL_Y_KEY = "PLAYER_LOCATION__LOCATION_INITIAL_Y_KEY";
+  private const LOCATION_PREVIOUS_X_KEY = "PLAYER_LOCATION__LOCATION_PREVOIUS_X_KEY";
+  private const LOCATION_PREVIOUS_Y_KEY = "PLAYER_LOCATION__LOCATION_PREVOIUS_Y_KEY";
 
   /**
    * @param State_Location $location
@@ -47,6 +49,25 @@ class PlayerLocationState extends BaseState {
     return $this->getPlayerInitialLocationCore();
   }
 
+  /**
+   * @param State_Location $location
+   * @return State_Location
+   */
+  public function setPlayerPreviousLocation(
+    object $location
+  ): object {
+    $this->writeState(self::LOCATION_PREVIOUS_X_KEY, $location->coordX);
+    $this->writeState(self::LOCATION_PREVIOUS_Y_KEY, $location->coordY);
+    return $this->getPlayerPreviousLocationCore();
+  }
+
+  /**
+   * @return State_Location
+   */
+  public function getPlayerPreviousLocation(): object
+  {
+    return $this->getPlayerPreviousLocationCore();
+  }
 
   /**
    * @return State_Location
@@ -67,6 +88,17 @@ class PlayerLocationState extends BaseState {
     return new State_Location(
       $this->readState(self::LOCATION_INITIAL_X_KEY),
       $this->readState(self::LOCATION_INITIAL_Y_KEY),
+    );
+  }
+
+  /**
+   * @return State_Location
+   */
+  public function getPlayerPreviousLocationCore(): object
+  {
+    return new State_Location(
+      $this->readState(self::LOCATION_PREVIOUS_X_KEY),
+      $this->readState(self::LOCATION_PREVIOUS_Y_KEY),
     );
   }
 
