b44e3ac873c5ab9a257df95a2750ac43cb75e9db
kfarkasHU <sohamar@farkask.eu>
2026-01-05 19:40:34 +0100

    fix(deploy ftp): add commas

--- .github/actions/deploy-ftp/deploy-ftp.sh
+++ .github/actions/deploy-ftp/deploy-ftp.sh
@@ -13,7 +13,7 @@ echo "Deploying files..."
 lftp -c "
 set ftp:list-options -a
 set ssl:verify-certificate no
-open -u $FTP_USER,$FTP_PASS $FTP_HOST
+open -u "$FTP_USER,$FTP_PASS" "$FTP_HOST"
 mirror -R $LOCAL_DIR $REMOTE_DIR  --only-newer --parallel=5 --verbose
 bye
 "
