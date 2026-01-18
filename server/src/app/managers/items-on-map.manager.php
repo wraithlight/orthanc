8bcaa7c43df4f5d47ec075c4247e9f033d9ffc37
kfarkasHU <sohamar@farkask.eu>
2026-01-18 13:37:51 +0100

    feat(items on map): add gold in chest
    Ref: #8

--- server/src/app/managers/items-on-map.manager.php
+++ server/src/app/managers/items-on-map.manager.php
@@ -9,7 +9,8 @@ class ItemsOnMapManager
   private const WEIGHT_ARMOR = 100;
   private const WEIGHT_BOW = 10;
 
-  private const GOLD_PERCENTAGE_ON_MAP = 2;
+  private const GOLD_PURE_PERCENTAGE_ON_MAP = 2;
+  private const GOLD_CHEST_PERCENTAGE_ON_MAP = 2;
   private const NUMBER_OF_ORBS = 1;
 
   private $_itemsService;
@@ -22,39 +23,33 @@ class ItemsOnMapManager
   public function createItemsOnMap(
     array $tileCoordinates
   ): void {
+    $numberOfTiles = count($tileCoordinates);
+
     $itemsInChests = [
-      [
-        "key" => "ITEM_CHEST_SWORD",
-        "weight" => self::WEIGHT_SWORD,
-        "pickedupLabel" => "You picked up a sword from the chest."
-      ],
-      [
-        "key" => "ITEM_CHEST_SHIELD",
-        "weight" => self::WEIGHT_SHIELD,
-        "pickedupLabel" => "You picked up a shield from the chest."
-      ],
-      [
-        "key" => "ITEM_CHEST_ARMOR",
-        "weight" => self::WEIGHT_ARMOR,
-        "pickedupLabel" => "You picked up an armor from the chest."
-      ],
-      [
-        "key" => "ITEM_CHEST_BOW",
-        "weight" => self::WEIGHT_BOW,
-        "pickedupLabel" => "You picked up a bow from the chest."
-      ],
+      ...$this->getItemsInChest(),
     ];
 
-    $numberOfTiles = count($tileCoordinates);
-    $numberOfGoldOnMap = $numberOfTiles / 100 * self::GOLD_PERCENTAGE_ON_MAP;
+    $chestGoldOnMap = $this->getGoldInChest(
+      $this->getRandomElements(
+        $this->getCountFromPercentage(self::GOLD_CHEST_PERCENTAGE_ON_MAP, $numberOfTiles),
+        $tileCoordinates
+      )
+    );
 
-    $gold = $this->getGoldItemsOnMap($numberOfGoldOnMap, $tileCoordinates);
+    $pureGoldOnMap = $this->getGoldItemsOnMap(
+      $this->getRandomElements(
+        $this->getCountFromPercentage(self::GOLD_PURE_PERCENTAGE_ON_MAP, $numberOfTiles),
+        $tileCoordinates
+      )
+    );
+
     $orb = $this->getOrbsOnMap(self::NUMBER_OF_ORBS, $tileCoordinates);
     $chests = $this->getChestsOnMap($itemsInChests, $tileCoordinates);
 
     $this->_itemsService->setItemsOnMap(
       [
-        ...$gold,
+        ...$pureGoldOnMap,
+        ...$chestGoldOnMap,
         ...$orb,
         ...$chests
       ]
@@ -125,12 +120,10 @@ class ItemsOnMapManager
    * @return Item[]
    */
   private function getGoldItemsOnMap(
-    int $number,
-    array $tileCoordinates
+    array $locations
   ): array {
     $itemsOnMap = [];
-    for ($i = 0; $i < $number; $i++) {
-      $randomIndex = array_rand($tileCoordinates, 1);
+    for ($i = 0; $i < count($locations); $i++) {
       $amount = roll_d150();
       array_push(
         $itemsOnMap,
@@ -144,12 +137,81 @@ class ItemsOnMapManager
           "Pick up Gold ($amount)",
           "You picked up Gold ($amount)",
           "You see a pile of gold.",
-          $tileCoordinates[$randomIndex]["x"],
-          $tileCoordinates[$randomIndex]["y"],
+          $locations[$i]["x"],
+          $locations[$i]["y"],
         )
       );
     }
     return $itemsOnMap;
+  }
+
+  private function getGoldInChest(array $locations): array {
+    $itemsOnMap = [];
+    for ($i = 0; $i < count($locations); $i++) {
+      $amount = roll_d150();
+      array_push(
+        $itemsOnMap,
+        createItem(
+          "ITEM_CHEST_GOLD",
+          "item_chest",
+          $amount,
+          $amount,
+          $amount * self::GOLD_WEIGHT,
+          true,
+          "Loot chest",
+          "You picked up Gold ($amount) from the chest",
+          "You see a chest",
+          $locations[$i]["x"],
+          $locations[$i]["y"],
+        )
+      );
+    }
+    return $itemsOnMap;
+  }
+
+  private function getItemsInChest(): array {
+    return [
+      [
+        "key" => "ITEM_CHEST_SWORD",
+        "weight" => self::WEIGHT_SWORD,
+        "pickedupLabel" => "You picked up a sword from the chest."
+      ],
+      [
+        "key" => "ITEM_CHEST_SHIELD",
+        "weight" => self::WEIGHT_SHIELD,
+        "pickedupLabel" => "You picked up a shield from the chest."
+      ],
+      [
+        "key" => "ITEM_CHEST_ARMOR",
+        "weight" => self::WEIGHT_ARMOR,
+        "pickedupLabel" => "You picked up an armor from the chest."
+      ],
+      [
+        "key" => "ITEM_CHEST_BOW",
+        "weight" => self::WEIGHT_BOW,
+        "pickedupLabel" => "You picked up a bow from the chest."
+      ],
+    ];
+  }
+
+  private function getRandomElements(
+    int $numberOfElements,
+    array $source
+  ): array {
+    $indices = array_rand($source, $numberOfElements);
+    $result = [];
+    foreach ($indices as $index) {
+        $result[] = $source[$index];
+    }
+
+    return $result;
+  }
+
+  private function getCountFromPercentage(
+    int $percentage,
+    int $base
+  ): int {
+    return $base / 100 * $percentage;
   }
 
 }
