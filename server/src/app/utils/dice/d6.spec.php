56ed61b3fb61fa5eaf7bc6bac7c6df39d997b00a
kfarkasHU <sohamar@farkask.eu>
2026-02-08 12:07:00 +0100

    chore(*): add phpunit unit tests
    Ref: #7

---
+++ server/src/app/utils/dice/d6.spec.php
@@ -0,0 +1,19 @@
+<?php
+
+use PHPUnit\Framework\TestCase;
+
+require_once __DIR__ . '/d6.php';
+
+class D6 extends TestCase
+{
+  public function testRollD6ReturnsIntBetween1And6(): void
+  {
+    for ($i = 0; $i < 100; $i++) {
+      $result = roll_d6();
+
+      $this->assertIsInt($result);
+      $this->assertGreaterThanOrEqual(1, $result);
+      $this->assertLessThanOrEqual(6, $result);
+    }
+  }
+}
