68e9cbcb811710c8704ac2742140a647bcd9b77e
kfarkasHU <sohamar@farkask.eu>
2026-02-16 21:35:59 +0100

    feat(*): run unit tests on F5
    Ref: #216

--- server/src/app/utils/dice/d6.spec.php
+++ server/src/app/utils/dice/d6.spec.php
@@ -6,6 +6,9 @@ require_once __DIR__ . '/d6.php';
 
 class D6 extends TestCase
 {
+  /**
+   * @testdox roll_d6() returns an int between 1 and 6
+   */
   public function testRollD6ReturnsIntBetween1And6(): void
   {
     for ($i = 0; $i < 100; $i++) {
