e6d62e90559e386b356b90b8f2f15b21549b66ba
kfarkasHU <sohamar@farkask.eu>
2026-02-17 20:18:01 +0100

    chore(*): rename `frontend` to `src-frontend`
    Ref: #192

--- run-dev.sh
+++ run-dev.sh
@@ -9,7 +9,7 @@ docker run -it --rm \
   -d \
   -p 3100:80 \
   -v $ROOT_DIR/server/src:/var/www/html \
-  -v $ROOT_DIR/frontend/public:/var/www/html/fe-assets \
+  -v $ROOT_DIR/src-frontend/public:/var/www/html/fe-assets \
   -v $ROOT_DIR/server/game-data:/var/www/game-data \
   -v $ROOT_DIR/docs-api:/var/www/docs-api \
   --env-file .env.dev \
