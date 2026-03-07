1a4e418a2abaf896da103f6c9f8e319d09b68b81
kfarkasHU <sohamar@farkask.eu>
2026-03-07 15:32:21 +0100

    feat(*): add HoF to Login screen
    Ref: #307

--- src-frontend/src/state/index.ts
+++ src-frontend/src/state/index.ts
@@ -4,8 +4,10 @@ import { Predicate } from "../framework";
 import { ApplicationConfigurationState } from "./application-configuration";
 import {
   EVENT_BACK_FROM_CHARACTER_CREATION,
+  EVENT_CLOSE_HALL_OF_FAME,
   EVENT_LOGIN_SUCCESS,
-  EVENT_NEXT_FROM_CHARACTER_CREATION
+  EVENT_NEXT_FROM_CHARACTER_CREATION,
+  EVENT_OPEN_HALL_OF_FAME
 } from "./events";
 import { STATE_NAME } from "./player-name";
 
@@ -15,6 +17,8 @@ export const State = {
   name: STATE_NAME,
   events: {
     loginSuccess: EVENT_LOGIN_SUCCESS,
+    openHallOfFame: EVENT_OPEN_HALL_OF_FAME,
+    closeHallOfFame: EVENT_CLOSE_HALL_OF_FAME,
     nextFromCharacterCreation: EVENT_BACK_FROM_CHARACTER_CREATION,
     backFromCharacterCreation: EVENT_NEXT_FROM_CHARACTER_CREATION,
   }
