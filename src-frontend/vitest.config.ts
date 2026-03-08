df56b285692cc09e82931c4fd2c38ca5327071ea
kfarkasHU <sohamar@farkask.eu>
2026-03-08 16:05:24 +0100

    chore(vitest): proper files and disable coverage thresholds
    Ref: #324

--- src-frontend/vitest.config.ts
+++ src-frontend/vitest.config.ts
@@ -7,11 +7,19 @@ export default defineConfig({
       provider: "v8",
       reporter: ["text", "html"],
       thresholds: {
-        statements: 85,
-        branches: 85,
-        functions: 85,
-        lines: 85
-      }
+        // statements: 85,          // TODO
+        // branches: 85,            // TODO
+        // functions: 85,           // TODO
+        // lines: 85                // TODO
+      },
+      include: ["src/**/*.ts"],
+      exclude: [
+        "src/**/*.selector.ts",
+        "src/**/*/index.ts",
+        "src/**/*.model.ts",
+        "src/**/*.type.ts",
+        "src/environment/**/*.ts",
+      ],
     }
   }
 });
