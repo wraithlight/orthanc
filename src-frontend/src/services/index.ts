f39471c0da949350c6d0cb4e757cd9110df0221d
kfarkasHU <sohamar@farkask.eu>
2026-03-09 19:21:00 +0100

    feat(services): add device service
    Ref: #297

--- src-frontend/src/services/index.ts
+++ src-frontend/src/services/index.ts
@@ -1,4 +1,5 @@
 export * from "./configuration.service";
+export * from "./device.service";
 export * from "./dialog-queue.service";
 export * from "./hall-of-fame.service";
 export * from "./keyboard-event.service";
