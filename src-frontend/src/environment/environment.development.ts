f9f07f146e26f20ddc53ec1a693c0e204d7ccd1a
kfarkasHU <sohamar@farkask.eu>
2026-03-12 22:12:14 +0100

    feat(environment): add `platform`
    Ref: #357

--- src-frontend/src/environment/environment.development.ts
+++ src-frontend/src/environment/environment.development.ts
@@ -2,5 +2,6 @@ import { IEnvironment } from "./environment.model";
 
 export const Environment: IEnvironment = {
   environmentName: "development",
-  apiBaseUrl: ""
+  apiBaseUrl: "",
+  platform: "web"
 }
