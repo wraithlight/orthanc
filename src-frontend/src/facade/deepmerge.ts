acd5832ada94616db0866a4dffa2fdeb086b90a5
kfarkasHU <sohamar@farkask.eu>
2026-03-19 20:52:08 +0100

    refactor(facade): add two generic type to facade

--- src-frontend/src/facade/deepmerge.ts
+++ src-frontend/src/facade/deepmerge.ts
@@ -1,8 +1,8 @@
 import { deepmerge as _deepmerge } from "deepmerge-ts";
 
-export function deepmerge<T>(
+export function deepmerge<T, U>(
   left: T,
-  right: T
+  right: U
 ) {
   _deepmerge(left, right);
 }
