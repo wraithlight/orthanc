b202c2ca345bf315b36e0f321bb07942a05a8d4d
kfarkasHU <sohamar@farkask.eu>
2026-03-20 21:21:41 +0100

    feat(*): add BE header validator MW
    Ref: #403

--- src-server/src/index.php
+++ src-server/src/index.php
@@ -95,6 +95,14 @@ require_once("./app/controllers/configuration.controller.php");
 
 $isSwadocEnabled = getenv("SWAGGER_ENABLED");
 
+if (!isHeaderValid(
+  getHeaderValue(HeaderName::Accept->value, ""),
+  [HeaderValueAccept::ApplicationJson->value])
+) {
+  http_response_code(400);
+  echo json_encode(createFailResponse(ErrorCode::ERROR_0400_H, "Invalid header (" .HeaderName::Accept->value . ")"));
+}
+
 use PhpApi2\PhpAPI2Wrapper as Wrapper;
 
 $loginControllerFactory = function () {
