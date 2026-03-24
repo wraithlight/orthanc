d2029549d1081009d0ef921529a05b4d7745970b
kfarkasHU <sohamar@farkask.eu>
2026-03-24 21:52:00 +0100

    feat(operation result): add payload to warning
    Ref: #54

--- src-frontend/src/framework/operation-result/operation-result-warning.ts
+++ src-frontend/src/framework/operation-result/operation-result-warning.ts
@@ -1,14 +1,16 @@
 import { OperationResultBase } from "./operation-result-base";
 
-export class OperationResultWarning extends OperationResultBase {
+export class OperationResultWarning<T = undefined> extends OperationResultBase {
   protected readonly severity = "WARNING";
   public readonly errors: ReadonlyArray<string>;
-
+  public readonly payload: T;
 
   constructor(
+    _payload: T,
     ..._errors: ReadonlyArray<string>
   ) {
     super();
     this.errors = _errors;
+    this.payload = _payload;
   }
 }
