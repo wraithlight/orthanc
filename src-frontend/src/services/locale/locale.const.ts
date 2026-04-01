bde42431a9e701cafd8d1a30a2ecb1252ffa053c
kfarkasHU <sohamar@farkask.eu>
2026-04-01 20:17:42 +0200

    feat(services): add locale service
    Ref: #449

---
+++ src-frontend/src/services/locale/locale.const.ts
@@ -0,0 +1,5 @@
+import { Locale } from "./locale.type";
+
+export const LOCALE_COOKIE_NAME = "orthanc_locale";
+export const LOCALE_COOKIE_EXPIRY_DAYS = 30;
+export const DEFAULT_LOCALE: Locale = "en";
