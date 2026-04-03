e482547033e62fb48295de9cebce8ccf316ce18a
kfarkasHU <sohamar@farkask.eu>
2026-04-03 14:46:19 +0200

    feat(header names): add platform version header
    Ref: #153

--- src-frontend/src/domain/header-names.const.ts
+++ src-frontend/src/domain/header-names.const.ts
@@ -1,6 +1,7 @@
 export const enum HeaderNames {
   Platform = "X-ORTHANC-PLATFORM",
   RequestId = "X-ORTHANC-REQUEST-ID",
+  PlatformVersion = "X-ORTHANC-PLATFORM-VERSION",
   ContentType = "Content-Type",
   Accept = "Accept",
 };
