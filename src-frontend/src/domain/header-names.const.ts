c5d95a0e659c562d56605ef81a0d3d784dc4195e
kfarkasHU <sohamar@farkask.eu>
2026-04-05 19:39:21 +0200

    feat(domain): add Device header
    Ref: #346

--- src-frontend/src/domain/header-names.const.ts
+++ src-frontend/src/domain/header-names.const.ts
@@ -2,6 +2,7 @@ export const enum HeaderNames {
   Platform = "X-ORTHANC-PLATFORM",
   RequestId = "X-ORTHANC-REQUEST-ID",
   PlatformVersion = "X-ORTHANC-PLATFORM-VERSION",
+  Device = "X-ORTHANC-DEVICE",
   ContentType = "Content-Type",
   Accept = "Accept",
 };
