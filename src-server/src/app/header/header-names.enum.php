5264d869c375891b9c6f8caa46f64ae4f92724f5
kfarkasHU <sohamar@farkask.eu>
2026-04-05 21:56:33 +0200

    feat(*): validate Device header
    Ref: #298

--- src-server/src/app/header/header-names.enum.php
+++ src-server/src/app/header/header-names.enum.php
@@ -2,5 +2,6 @@
 enum HeaderName: string {
   case Accept = "HTTP_ACCEPT";
   case RequestId = "HTTP_X_ORTHANC_REQUEST_ID";
+  case Device = "HTTP_X_ORTHANC_DEVICE";
   case OrthancPlafromVersion = "X-ORTHANC-PLATFORM-VERSION";
 }
