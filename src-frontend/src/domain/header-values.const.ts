2d1a174108363098c0c998b8b44dc7b10f6276a9
kfarkasHU <sohamar@farkask.eu>
2026-04-05 19:47:09 +0200

    style(domain): add knip ignore
    Ref: #346

--- src-frontend/src/domain/header-values.const.ts
+++ src-frontend/src/domain/header-values.const.ts
@@ -8,6 +8,9 @@ export const enum HeaderValueAccept {
   ApplicationJson = APPLICATION_JSON,
 }
 
+/**
+ * @public It's used but for some reason knip reports it as unused.
+ */
 export const enum Device {
   Mobile = "mobile",
   Desktop = "desktop",
