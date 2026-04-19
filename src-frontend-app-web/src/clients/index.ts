1c6cb7eb2ce3d66241f42cf7f62adc0357750271
kfarkasHU <sohamar@farkask.eu>
2026-04-19 21:43:29 +0200

    refactor(*): move `LoginClients` to `clients`

--- src-frontend-app-web/src/clients/index.ts
+++ src-frontend-app-web/src/clients/index.ts
@@ -2,3 +2,4 @@ export * from "./character-creation.client";
 export * from "./configuration.client";
 export * from "./hall-of-fame.client";
 export * from "./localization.client";
+export * from "./login.client";
