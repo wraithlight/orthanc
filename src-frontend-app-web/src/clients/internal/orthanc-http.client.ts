6ccf894b5b3976c0929f298e7893cd9eafce3c24
kfarkasHU <sohamar@farkask.eu>
2026-05-15 19:31:00 +0100

    feat(internal): add `OrthancHttpClient`
    Ref: #54

---
+++ src-frontend-app-web/src/clients/internal/orthanc-http.client.ts
@@ -0,0 +1,44 @@
+import { HeaderNames } from "../../domain";
+import { Environment } from "../../environment";
+import { newGuid } from "../../framework";
+import { InterceptorCache } from "../../http";
+import { RuntimeContext } from "../../runtime-context";
+import { JsonHttpClient } from "./json-http.client";
+
+export abstract class OrthancHttpClient extends JsonHttpClient {
+
+  constructor(
+    protected readonly _baseUrl: string
+  ) {
+    super(_baseUrl);
+  }
+
+  protected async beforeRequest(
+    _url: string,
+    _init: RequestInit
+  ): Promise<boolean> {
+    return true;
+  }
+
+  protected async afterRequest(response: Response): Promise<boolean> {
+    const interceptors = InterceptorCache.getInstance().getAfterInterceptors();
+    interceptors.forEach(m => m(response));
+    return true;
+  }
+
+  protected getHttpHeaders(): HeadersInit {
+    return {
+      ...super.getJsonHeaders(),
+      ...this.getOrthancHeaders()
+    }
+  }
+
+  private getOrthancHeaders(): HeadersInit {
+    return {
+      [HeaderNames.Device]: RuntimeContext.device,
+      [HeaderNames.Platform]: Environment.platform,
+      [HeaderNames.RequestId]: newGuid(),
+    };
+  }
+
+}
