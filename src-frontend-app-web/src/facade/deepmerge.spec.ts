83134c39ec348655113662e339217138be6f3f87
kfarkasHU <sohamar@farkask.eu>
2026-06-22 04:18:00 +0100

    refactor: proper description

--- src-frontend-app-web/src/facade/deepmerge.spec.ts
+++ src-frontend-app-web/src/facade/deepmerge.spec.ts
@@ -5,12 +5,12 @@ import { vi, describe, it, expect } from "vitest";
 import { deepmerge } from "./deepmerge";
 import { deepmerge as deepmergeSpy } from "deepmerge-ts";
 
-describe("isNilSpecs", () => {
+describe("deepmergeSpecs", () => {
 
   const MOCK_LEFT = { a: [1], b: 2 };
   const MOCK_RIGHT = { a: [2], c: 3 }
 
-  it("must call isNilCore", () => {
+  it("must call deepmerge with the correct arguments", () => {
     deepmerge(MOCK_LEFT, MOCK_RIGHT);
     expect(deepmergeSpy).toHaveBeenCalled();
     expect(deepmergeSpy).toHaveBeenCalledTimes(1);
