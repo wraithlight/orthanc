688d444cb32ae6e9a031ec319917674b7735f0d1
kfarkasHU <sohamar@farkask.eu>
2026-08-30 16:28:00 +0100

    chore: ignore generated DAL in eslint

--- src-frontend-app-web/eslint.config.js
+++ src-frontend-app-web/eslint.config.js
@@ -31,7 +31,7 @@ const COMMON_RULES_ERROR = {
 export default [
   ...tseslint.configs.recommended,
   {
-    ignores: ["dist/**", "node_modules/**"],
+    ignores: ["dist/**", "node_modules/**", "src/dal-generated/**"],
   },
   {
     files: ["**/*.ts"],
